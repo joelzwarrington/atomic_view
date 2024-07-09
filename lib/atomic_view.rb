3aa01348e0b4a35febcb54f01561f52012b64c6d
Joel Warrington <joelw@hey.com>
2024-07-08 22:34:43 -0600

    Add method to get preview folder path

--- lib/atomic_view.rb
+++ lib/atomic_view.rb
@@ -2,4 +2,12 @@
 
 require_relative "atomic_view/version"
 
-module AtomicView; end
+# Component library built for Ruby on Rails with first-class support for ActionView using ViewComponent.
+module AtomicView
+  class << self
+    def preview_folder_path
+      root_directory = File.dirname(__dir__)
+      File.join(root_directory, "components", "previews")
+    end
+  end
+end
