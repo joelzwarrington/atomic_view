26660442962251aab713926748c0c0194905d12f
Joel Warrington <joelw@hey.com>
2024-10-20 12:53:36 -0600

    Update styling of email field

--- lib/atomic_view/components/email_field_component.rb
+++ lib/atomic_view/components/email_field_component.rb
@@ -1,6 +1,17 @@
 module AtomicView
   module Components
     class EmailFieldComponent < ViewComponent::Form::EmailFieldComponent
+      def call
+        render TextFieldComponent.new(
+          @form,
+          @object_name,
+          @object_method,
+          @options.merge(
+            type: "email",
+            left_section: icon("at-symbol", variant: :mini, options: {class: "size-5 text-gray-500"})
+          )
+        )
+      end
     end
   end
 end
