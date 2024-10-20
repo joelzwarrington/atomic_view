856d3a989897bc6445a3e857be5ae6dfabe99c14
Joel Warrington <joelw@hey.com>
2024-10-20 13:50:16 -0600

    Add submit styling

--- lib/atomic_view/components/submit_component.rb
+++ lib/atomic_view/components/submit_component.rb
@@ -1,6 +1,9 @@
 module AtomicView
   module Components
     class SubmitComponent < ViewComponent::Form::SubmitComponent
+      def html_class
+        "cursor-pointer rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700"
+      end
     end
   end
 end
