53e1b35f9e0f954f75af0ed68b13ad2f7069c464
Joel Warrington <joelw@hey.com>
2025-01-14 16:08:11 -0700

    Update dark mode styling on date field

--- lib/atomic_view/components/date_field_component.rb
+++ lib/atomic_view/components/date_field_component.rb
@@ -3,10 +3,13 @@ module AtomicView
     class DateFieldComponent < ViewComponent::Form::DateFieldComponent
       def html_class
         class_names(
-          *%W[block w-full h-9 min-w-0 flex-1 rounded-md border-0 py-1 text-sm shadow-sm ring-1],
-          "disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200",
-          "text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-neutral-700",
-          "text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500" => method_errors?
+          class_names(
+            *%W[block w-full h-9 min-w-0 flex-1 rounded-md border-0 py-1 text-sm shadow-sm ring-1],
+            "disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-zinc-500 disabled:ring-zinc-200",
+            "bg-transparent dark:bg-white/5 text-zinc-950 ring-zinc-950/10 dark:ring-white/10 placeholder:text-zinc-500 dark:text-white focus:ring-neutral-700 focus:border-zinc-950/20 dark:focus:ring-white/20",
+            "text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500" => method_errors?,
+          ),
+          options[:class]
         )
       end
     end
