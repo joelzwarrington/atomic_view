cc65571d8ecf637c8c82fd7898b1e167ca67a991
Joel Warrington <joelw@hey.com>
2025-01-14 01:24:14 -0700

    More dark styling

--- lib/atomic_view/components/label_component.rb
+++ lib/atomic_view/components/label_component.rb
@@ -2,7 +2,7 @@ module AtomicView
   module Components
     class LabelComponent < ViewComponent::Form::LabelComponent
       def html_class
-        "block text-sm font-medium leading-6 text-gray-900"
+        "block text-sm font-medium leading-6 text-zinc-950 dark:text-white"
       end
     end
   end
--- lib/atomic_view/components/select_component.rb
+++ lib/atomic_view/components/select_component.rb
@@ -5,8 +5,8 @@ module AtomicView
         class_names(
           class_names(
             *%W[block w-full h-9 min-w-0 flex-1 rounded-md border-0 py-1 text-sm shadow-sm ring-1],
-            "disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200",
-            "text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-neutral-700",
+            "disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-zinc-500 disabled:ring-zinc-200",
+            "bg-transparent dark:bg-white/5 text-zinc-950 ring-zinc-950/10 dark:ring-white/10 placeholder:text-zinc-500 dark:text-white focus:ring-neutral-700 focus:border-zinc-950/20 dark:focus:ring-white/20",
             "text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500" => method_errors?,
           ),
           options[:class]
