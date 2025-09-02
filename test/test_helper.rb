0095838492028ba76321eac13af5ea590e57993d
Joel Warrington <joelw@hey.com>
2025-09-02 14:29:04 -0600

    Update test helper to include ViewComponent testing utilities

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,19 @@ ActiveRecord::Migrator.migrations_paths = [File.expand_path('../test/dummy/db/mi
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../db/migrate', __dir__)
 require 'rails/test_help'
 
+# Load ViewComponent test helpers
+require 'view_component/test_helpers'
+
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.fixture_paths = [File.expand_path('fixtures', __dir__)]
   ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
   ActiveSupport::TestCase.file_fixture_path = "#{File.expand_path('fixtures', __dir__)}/files"
   ActiveSupport::TestCase.fixtures :all
+end
+
+# Include ViewComponent test helpers and ActionView helpers
+class ViewComponent::TestCase
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::TextHelper
 end
